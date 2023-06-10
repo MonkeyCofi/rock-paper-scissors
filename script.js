b77823705daeba56ae1a3233be2852f5ddcfcf86
MonkeyCofi <piercediet06@gmail.com>
2023-06-10 15:11:02 +0400

    Modify playRound function

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const start = document.querySelector('.start');
+
 const rock = 'rock';
 const paper = 'paper';
 const scissors = 'scissors';
+const win = 'You win.'
+const tie = `It's a tie.`
+const loss = 'You lose.'
+
 
 // Get the player's move and return it in lowercase
 function playerMove() {
@@ -23,47 +28,74 @@ function computerMove() {
     return computerMove;
 }
 
-/* 
-    Get both user and computer moves and store in a variable
-    
-*/
 function playRound(playerMove, computerMove) {
     playerMove = playerMove()
     computerMove = computerMove()
+    
+    let result = ''
     // rock
+    // if (playerMove === rock){
+    //     if (computerMove === rock){
+    //         return `It's a tie`
+    //     } else if (computerMove === paper){
+    //         return `You lose. Computer chose paper`
+    //     } else if (computerMove === scissors){
+    //         return `You win. Computer chose scissors`
+    //     }
+    // // Paper
+    // } else if (playerMove === paper){
+    //     if (computerMove === rock){
+    //         return `You win. Computer chose rock`
+    //     } else if (computerMove === paper){
+    //         return `It's a tie`
+    //     } else if (computerMove === scissors){
+    //         return `You lose. Computer chose scissors`
+    //     }
+    // // Scissors
+    // } else {
+    //     if (computerMove === rock){
+    //         return `You lose. Computer chose rock`
+    //     } else if (computerMove === paper){
+    //         return `You win. Computer chose paper`
+    //     } else if (computerMove === scissors){
+    //         return `It's a tie`
+    //     }
+    // }
+
     if (playerMove === rock){
         if (computerMove === rock){
-            return `It's a tie`
+            return tie;
         } else if (computerMove === paper){
-            return `You lose. Computer chose paper`
-        } else if (computerMove === scissors){
-            return `You win. Computer chose scissors`
+            return loss;
+        } else {
+            return win;
         }
-    // Paper
     } else if (playerMove === paper){
         if (computerMove === rock){
-            return `You win. Computer chose rock`
+            return win;
         } else if (computerMove === paper){
-            return `It's a tie`
-        } else if (computerMove === scissors){
-            return `You lose. Computer chose scissors`
+            return tie;
+        } else {
+            return loss;
         }
-    // Scissors
-    } else {
+    } else if (playerMove === scissors){
         if (computerMove === rock){
-            return `You lose. Computer chose rock`
+            return loss
         } else if (computerMove === paper){
-            return `You win. Computer chose paper`
-        } else if (computerMove === scissors){
-            return `It's a tie`
+            return win;
+        } else {
+            return tie;
         }
     }
 }
 
 function game(playerMove, computerMove) {
     let wins = losses = draws = 0;
+    for (let rounds = 1; rounds <= 5; rounds++) {
+
+    }
 }
 
 start.addEventListener('click', ()=> {
-    console.log(playRound(playerMove, computerMove));
+    alert(playRound(playerMove, computerMove));
 })
